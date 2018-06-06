508efc68459f50127eaa2102672767cc5010cd56
Antoine Musso <hashar@free.fr>
2018-06-06 15:12:26 +0200

    Remove SQL COMMENT
    Does not work with SQLite and MediaWiki. Use inline comments instead.
    
    Change-Id: I245570c2bbebf7d5b0d194709fa682043951bd79

--- sql/spelling_dictionary.sql
+++ sql/spelling_dictionary.sql
@@ -2,7 +2,13 @@
 
 CREATE TABLE IF NOT EXISTS /*_*/spell_dict_word_list (
 	sd_word varchar(50) NOT NULL,
-	sd_language varchar(20) NOT NULL COMMENT 'Language to which the word belongs',
+
+	-- Language to which the word belongs
+	sd_language varchar(20) NOT NULL,
+
 	sd_timestamp varbinary(14) NOT NULL,
-	sd_user varchar(40) NOT NULL COMMENT 'Name of the user who submitted the word'
+
+	-- Name of the user who submitted the word
+	sd_user varchar(40) NOT NULL
+
 )/*$wgDBTableOptions*/;
